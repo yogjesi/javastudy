974e5f34e3844ce71049c15407f338d3ed3f6092
서지원 <yogjesi@naver.com>
2023-02-01 02:52:21 +0900

    jiwon_230131

--- Javastudy/src/ch5/Alone_jiwon.java
+++ Javastudy/src/ch5/Alone_jiwon.java
@@ -1,37 +1,11 @@
 package ch5;
 
 public class Alone_jiwon {
-	public static void main(String[] args) {
-		int num1 = 10;
-		int num2 = 20;
-		
-		int sum = add(num1, num2);
-		System.out.println(num1 + " + " + num2 + " = " + sum + "입니다.");
-	}
+	String name;
+	float height;
+	float weight;
 	
-	public static int add(int n1, int n2) {
-		int result = n1 + n2;
-		return result;
-	}
-	
-	public static int dim(int n1, int n2) {
-		int result = n1 - n2;
-		return result;
-	}
-	
-	public static int devide(int n1, int n2) {
-		if (n2 == 0) {
-			int result = -1;
-			return result;
-		}
-		else {
-			int result = n1/n2;
-			return result;
-		}
-	}
-	
-	public static int multiple(int n1, int n2) {
-		int result = n1*n2;
-		return result;
+	public Alone_jiwon(String pname) {
+		name = pname;
 	}
 }
--- Javastudy/src/ch5/Q4_jiwon.java
+++ Javastudy/src/ch5/Q4_jiwon.java
@@ -1,11 +1,13 @@
 package ch5;
 
 public class Q4_jiwon {
-
+	// 해당 클래스는 문제 속 'MyDateTest' 클래스를 대신함.
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 
-
+		MyDate date1 = new MyDate(30, 2, 2000);
+		System.out.println(date1.isValid());
+		
+		MyDate date2 = new MyDate(2, 10, 2006);
+		System.out.println(date2.isValid());
 	}
 
 }
