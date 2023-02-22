854fd35f2e6fe016f2aca077a9d05a09d731c708
서지원 <yogjesi@naver.com>
2023-02-23 04:43:02 +0900

    jiwon_230222

--- Javastudy/src/ch7/Q5_jiwon.java
+++ Javastudy/src/ch7/Q5_jiwon.java
@@ -4,7 +4,22 @@ public class Q5_jiwon {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//		Student studentLee = new Student(1001, "Lee");
+//		studentLee.addSubject("국어", 100);
+//		studentLee.addSubject("수학", 50);
+//		
+//		Student studentKim = new Student(1002, "Kim");
+//		studentKim.addSubject("국어", 70);
+//		studentKim.addSubject("수학", 85);
+//		studentKim.addSubject("영어", 100);
+//		
+//		studentLee.showStudentInfo();
+//		System.out.println("===========================");
+//		studentKim.showStudentInfo();
+		
+		Dog dog1 = new Dog("라라", "래브라도");
+//		dog1.addDog("","");
+		
 	}
 
 }
