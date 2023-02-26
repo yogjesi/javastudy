65b776fdc9b8e63f6e8eb87fc0d057baef98c72f
ujumom <amethyst270@naver.com>
2023-02-26 10:57:33 +0900

    wooyoung_230226_ch12

---
+++ Javastudy/src/ch12/Q5_wooyoung/Student.java
@@ -0,0 +1,36 @@
+package ch12.Q5_wooyoung;
+
+public class Student {
+	
+	String name;
+	String id;
+
+	public Student(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Integer.parseInt(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Student) {
+			Student std = (Student)obj;
+			if(this.id == std.id) 
+				return true;
+			else 
+				return false;
+		}
+		return false;
+	}
+
+	@Override
+	public String toString() {
+
+		return id +":"+name;
+	}
+}
+	
---
+++ Javastudy/src/ch12/Q5_wooyoung/Test.java
@@ -0,0 +1,15 @@
+package ch12.Q5_wooyoung;
+import java.util.HashSet;
+public class Test {
+	public static void main(String[] args) {
+		HashSet<Student> set = new HashSet<Student>( );
+		
+		set.add(new Student("100", "홍길동"));
+		set.add(new Student("200", "강감찬"));
+		set.add(new Student("300", "이순신"));
+		set.add(new Student("400", "정약용"));
+		set.add(new Student("100", "송중기"));
+		
+		System.out.println(set);
+	}
+}
