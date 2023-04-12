f66f4f0e503d13ecc5235ec6607943e020fbdfcf
서지원 <yogjesi@naver.com>
2023-04-12 14:59:35 +0900

    jiwon_230412

---
+++ Javastudy/src/ch11/EqualTest.java
@@ -0,0 +1,58 @@
+package ch11;
+
+class Student{
+	int studentId;
+	String studentName;
+	
+	public Student(int studentId, String studentName) {
+		this.studentId = studentId;
+		this.studentName = studentName;
+	}
+	
+	public String toString() {
+		return studentId + "," + studentName;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Student) {
+			Student std = (Student)obj;
+			if(this.studentId == std.studentId)
+				return true;
+			else return false;
+		}
+		return false;
+	}
+	
+	
+}
+
+public class EqualTest {
+	public static void main(String[] args) {
+		Student studentLee = new Student(100, "이상원");
+		Student studentLee2 = studentLee;
+		Student studentSang = new Student(100, "이상원");
+		
+		if(studentLee == studentLee2)
+			System.out.println("studentLee와 studentLee2의 주소는 같습니다.");
+		else
+			System.out.println("studentLee와 studentLee2의 주소는 다릅니다.");
+		
+		if(studentLee.equals(studentLee2))
+			System.out.println("studentLee와 studentLee2는 동일합니다.");
+		else
+			System.out.println("studentLee와 studentLee2는 동일하지 않습니다.");
+		
+		if(studentLee == studentSang)
+			System.out.println("studentLee와 studentSang의 주소는 같습니다.");
+		else
+			System.out.println("studentLee와 studentSang의 주소는 다릅니다.");
+		
+		if(studentLee.equals(studentSang))
+			System.out.println("studentLee와 studentSang는 동일합니다.");
+		else
+			System.out.println("studentLee와 studentSang는 동일하지 않습니다.");
+	
+
+	}
+}
