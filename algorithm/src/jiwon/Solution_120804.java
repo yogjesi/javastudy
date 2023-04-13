370d6cad0311584be03c91c55d731d1bf5fa739d
서지원 <yogjesi@naver.com>
2023-04-13 21:17:52 +0900

    230413_jiwon_1

---
+++ algorithm/src/jiwon/Solution_120804.java
@@ -0,0 +1,10 @@
+package jiwon;
+
+public class Solution_120804 {
+    public int solution(int num1, int num2) {
+    	// 두 수의 곱
+        int answer = 0;
+        answer = num1*num2;
+        return answer;
+    }
+}
---
+++ algorithm/src/jiwon/Solution_120820.java
@@ -0,0 +1,10 @@
+package jiwon;
+
+public class Solution_120820 {
+    public int solution(int age) {
+    	// 나이 출력
+        int answer = 0;
+        answer = 2023 - age;
+        return answer;
+    }
+}
